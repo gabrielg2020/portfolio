07d1623c4c544f91b949069d0350e8d2ddf82dd9
Gabriel Guimaraes <gabriel.mg04@outlook.com>
2023-11-14 13:17:34 +0000

    HTML gets injected depending on selected nav item

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const navItems = document.querySelectorAll('.nav-item');
+const infoContainer = document.querySelector('#info-container');
 
 navItems.forEach(item => {
   item.addEventListener('click', () => {
     navItems.forEach(item => item.classList.remove('selected'));
     item.classList.add('selected');
+
+    if (item.innerHTML === 'skills') {
+      infoContainer.innerHTML = `
+      <ul id="skills-carousel">
+          <li><img src="/public/react.svg" alt=""></li>
+          <li><img src="/public/language-javascript.svg" alt=""></li>
+          <li><img src="/public/language-python.svg" alt=""></li>
+          <li><img src="/public/language-html5.svg" alt=""></li>
+          <li><img src="/public/language-css3.svg" alt=""></li>
+        </ul>
+      `;
+    } else {
+      infoContainer.innerHTML = 'not added yet'
+    }
   });
 });
