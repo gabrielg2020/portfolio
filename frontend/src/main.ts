b23cf8477a38ce0e87e89fb188afffbbf2de8da5
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2025-03-21 22:00:27 +0000

    chore: move to svelte-ts
    Signed-off-by: Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,8 @@
 import { mount } from 'svelte'
-import './styles/font.css'
-import './styles/global.css'
 import App from './App.svelte'
 
 const app = mount(App, {
-  target: document.getElementById('app'),
+  target: document.getElementById('app')!,
 })
 
 export default app
