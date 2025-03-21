4502954202cbe794bbf3d5c56821f178bb2f3959
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2025-03-21 18:11:17 +0000

    chore: track `Background.svelte`
    Signed-off-by: Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>

---
+++ frontend/src/components/Background.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  // Props
+  export let backgroundColor = "#121212";
+
+  let canvas: HTMLCanvasElement;
+  let ctx: CanvasRenderingContext2D;
+  let width: number;
+  let height: number;
+
+  onMount(() => {
+    // Initialise Canvas
+    ctx = canvas.getContext("2d");
+    resizeCanvas();
+
+    // Add event listeners
+    window.addEventListener("resize", resizeCanvas);
+
+    return () => {
+      window.removeEventListener("resize", resizeCanvas);
+    };
+  });
+
+  function resizeCanvas() {
+    const container = canvas.parentElement;
+    width = container.clientWidth;
+    height = container.clientHeight;
+    canvas.width = width;
+    canvas.height = height;
+  }
+</script>
+
+<div class="container">
+  <canvas bind:this={canvas}></canvas>
+  <slot />
+</div>
+
+<style>
+</style>
