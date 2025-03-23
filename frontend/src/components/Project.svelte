f7381ad9ab27270dd2fac2ed39c42e46971e84e7
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2025-03-23 22:28:42 +0000

    feat: hit the poject endpoint and display dynamically
    Signed-off-by: Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>

--- frontend/src/components/Project.svelte
+++ frontend/src/components/Project.svelte
@@ -2,8 +2,8 @@
   export let title: string = "Project Name";
   export let description: string =
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultricies placerat interdum. Vivamus sed velit mattis, mattis lectus sed, maximus dolor. Quisque pulvinar tincidunt lacus, quis cursus nibh pretium eget. Proin porta mi at pulvinar maximus.";
-  export let langauges: string[] = ["go", "ts", "docker", "svelte"];
-  export let technologies: string[] = ["docker", "svelte", "docker", "svelte"];
+  export let langauges: string[] = ["go", "ts"];
+  export let technologies: string[] = ["docker", "svelte"];
 </script>
 
 <div class="project">
@@ -18,15 +18,15 @@
       <h4>Languages</h4>
       <div class="image-wrapper">
         {#each langauges as language (language)}
-        <img alt="logo of {language}" src="images/{language}.svg"/>
-      {/each}
+          <img alt="logo of {language}" src="images/{language}.svg" />
+        {/each}
       </div>
     </div>
     <div class="footer-wrapper">
       <h4>Technologies</h4>
       <div class="image-wrapper">
         {#each technologies as technology (technology)}
-          <img alt="logo of {technology}" src="/images/{technology}.svg"/>
+          <img alt="logo of {technology}" src="/images/{technology}.svg" />
         {/each}
       </div>
     </div>
@@ -57,5 +57,4 @@
   .image-wrapper > img {
     width: 100%;
   }
-  
 </style>
--- frontend/src/pages/Projects.svelte
+++ frontend/src/pages/Projects.svelte
@@ -1,5 +1,31 @@
 <script lang="ts">
   import Project from "../components/Project.svelte";
+
+  import { onMount } from "svelte";
+
+  let projects: { id:number, description: string; githubLink: string; title: string }[] =
+    [];
+  let error: string | null = null;
+
+  onMount(async () => {
+    try {
+      console.log("Requesting");
+      const response = await fetch("/api/projects");
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      projects = data.projects;
+      console.log(projects);
+    } catch (err) {
+      console.error("Failed to fetch projects:", err);
+      if (err instanceof Error) {
+        error = err.message;
+      } else {
+        error = String(err);
+      }
+    }
+  });
 </script>
 
 <div id="projects">
@@ -8,7 +34,15 @@
     <h4>Open them up to learn more!</h4>
   </div>
   <div class="projects-wrapper">
-    <Project />
+    {#if error}
+      <div class="error">{error}</div>
+    {:else}
+      <div class="projects-wrapper">
+        {#each projects as project (project.id)}
+          <Project title={project.title} description={project.description} />
+        {/each}
+      </div>
+    {/if}
   </div>
 </div>
 
@@ -26,13 +60,15 @@
     align-items: center;
     margin-bottom: 3vh; /* Space between header and projects */
   }
-  
+
   .projects-wrapper {
-    max-width: 16rem;
+    display: flex;
+
   }
 
-  h1, h4 {
-    font-family: 'Martian Mono', monospace;
+  h1,
+  h4 {
+    font-family: "Martian Mono", monospace;
     margin: 0;
   }
 
