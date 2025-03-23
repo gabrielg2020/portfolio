f2399821dfccf5f12de4eec7525caea500c3b234
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2025-03-23 03:23:26 +0000

    fix: serve static images
    Signed-off-by: Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -21,6 +21,7 @@ func SetupRouter() *gin.Engine {
 
 	// Serve static files
 	router.Static("/assets", "./static/assets")
+	router.Static("/images", "./static/assets")
 
 	// Serve index.html for any unmatched routes
 	router.NoRoute(func(ctx *gin.Context) {
