d14ae6fcd6edfbdf621c2667cdd08763030e2df4
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2025-03-24 16:48:45 +0000

    fix: specifiy project id
    Signed-off-by: Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>

--- backend/database/data/01_projects.sql
+++ backend/database/data/01_projects.sql
@@ -1,42 +1,42 @@
 BEGIN TRANSACTION;
 
--- goto
+-- 1 - goto
 INSERT INTO projects (title, description, github_link)
 VALUES ('goto', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultricies placerat interdum. Vivamus sed velit mattis, mattis lectus sed, maximus dolor. Quisque pulvinar tincidunt lacus, quis cursus nibh pretium eget. Proin porta mi at pulvinar maximus.', 'https://github.com/gabrielg2020');
 
 INSERT INTO project_languages (project_id, language_id, position)
 VALUES 
-    (last_insert_rowid(), 1, 1); -- go
+    (1, 1, 1); -- go
 
--- chess-api
+-- 2 - chess-api
 INSERT INTO projects (title, description, github_link)
 VALUES ('ChessAPI','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultricies placerat interdum. Vivamus sed velit mattis, mattis lectus sed, maximus dolor. Quisque pulvinar tincidunt lacus, quis cursus nibh pretium eget. Proin porta mi at pulvinar maximus.', 'https://github.com/gabrielg2020');
 
 INSERT INTO project_languages (project_id, language_id, position)
 VALUES 
-    (last_insert_rowid(), 1, 1); -- go
+    (2, 1, 1); -- go
 
--- maze-visualiser
+-- 3 - maze-visualiser
 INSERT INTO projects (title, description, github_link)
 VALUES ('Maze Visualiser','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultricies placerat interdum. Vivamus sed velit mattis, mattis lectus sed, maximus dolor. Quisque pulvinar tincidunt lacus, quis cursus nibh pretium eget. Proin porta mi at pulvinar maximus.', 'https://github.com/gabrielg2020');
 
 INSERT INTO project_languages (project_id, language_id, position)
 VALUES 
-    (last_insert_rowid(), 5, 1); -- vbnet
+    (3, 5, 1); -- vbnet
 
--- portfolio
+-- 4- portfolio
 INSERT INTO projects (title, description, github_link)
 VALUES ('Portfolio','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultricies placerat interdum. Vivamus sed velit mattis, mattis lectus sed, maximus dolor. Quisque pulvinar tincidunt lacus, quis cursus nibh pretium eget. Proin porta mi at pulvinar maximus.', 'https://github.com/gabrielg2020');
 
 INSERT INTO project_languages (project_id, language_id, position)
 VALUES 
-    (last_insert_rowid(), 1, 1), -- go
-    (last_insert_rowid(), 2, 2), -- typescript
-    (last_insert_rowid(), 4, 3); -- sql
+    (4, 1, 1), -- go
+    (4, 2, 2), -- typescript
+    (4, 4, 3); -- sql
 
 INSERT INTO project_technologies (project_id, technology_id, position)
 VALUES 
-    (last_insert_rowid(), 1, 1), -- svelte
-    (last_insert_rowid(), 2, 2); -- docker
+    (4, 1, 1), -- svelte
+    (4, 2, 2); -- docker
 
 COMMIT;
