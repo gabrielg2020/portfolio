425a74e9192fdafc131600ecf35f8da133ba3618
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2025-03-24 20:12:15 +0000

    feat(database): update `rebuild_db.sh`
    Signed-off-by: Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>

--- backend/database/rebuild_db.sh
+++ backend/database/rebuild_db.sh
@@ -1,24 +1,42 @@
 #!/bin/sh
+echo ""
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color
 
 database_file=database/portfolio.db
 
 # Run SQL script to rebuild schema
 schema_file=database/schema.sql
-echo "Rebuilding database schema, using $schema_file"
-sqlite3 "$database_file" < $schema_file
+echo "${CYAN}Rebuilding database schema${NC}"
+echo "  - Running schema: $schema_file"
+if ! output=$(sqlite3 "$database_file" < "$schema_file" 2>&1); then
+  echo "    - Error in $schema_file"
+  echo ""
+  echo "\033[0;31m$output\033[0m"
+  exit 1
+fi
 
 echo ""
 
 # Run database patches to insert data
 for dir in database/data/*/; do
-  echo "Processing patch version: $dir"
+  echo "${CYAN}Processing patch version: $dir${NC}"
   for sql_file in "$dir"*.sql; do
     if [ -f "$sql_file" ]; then
       echo "  - Running SQL file: $sql_file"
-      sqlite3 "$database_file" < "$sql_file"
+      if ! output=$(sqlite3 "$database_file" < "$sql_file" 2>&1); then
+        echo "    - Error in $sql_file"
+        echo ""
+        echo "${RED}$output${NC}"
+        exit 1
+      fi
     fi
   done
 done
 
-echo " "
-echo "Success!"
+echo ""
+echo "${GREEN}Success!${NC}"
+echo ""
