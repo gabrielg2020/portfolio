a23adda041e90ebefaf0d7d6b67654c3f81eab4f
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2025-03-24 19:25:22 +0000

    feat: update handler to serve valid json data
    Signed-off-by: Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>

--- backend/handlers/handlers.go
+++ backend/handlers/handlers.go
@@ -7,6 +7,8 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/gabrielg2020/backend/queries"
+	"github.com/gabrielg2020/backend/utils"
 	"github.com/gin-gonic/gin"
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -34,38 +36,57 @@ func GetProjects(c *gin.Context) {
 	db := openDatabase()
 	defer db.Close()
 
-	// Query database
-	rows, err := db.Query("SELECT project_id, title, description, github_link FROM projects")
+	var projects []map[string]interface{}
+
+	// Get Project query
+	query := queries.GetProjects(4)
+
+	rows, err := db.Query(query)
+
 	if err != nil {
 		log.Fatalf("Failed to query projects: %v", err)
 	}
 	defer rows.Close()
 
-	var projects []map[string]interface{}
-
-	// Grab all projects
+	// Grab projects
 	for rows.Next() {
 		var projectID, title, description, githubLink string
-		if err := rows.Scan(&projectID, &title, &description, &githubLink); err != nil {
+		var languagesStr, technologiesStr sql.NullString
+
+		if err := rows.Scan(&projectID, &title, &description, &githubLink, &languagesStr, &technologiesStr); err != nil {
 			log.Fatalf("Failed to scan project: %v", err)
 		}
-		project := map[string]interface{}{
-			"id":          projectID,
-			"title":       title,
-			"description": description,
-			"githubLink":  githubLink,
+
+		// Parse string 'arrays'
+		var languages, technologies []string
+		if languagesStr.Valid {
+			languages = utils.ParseStringSlice(languagesStr.String)
 		}
+		if technologiesStr.Valid {
+			technologies = utils.ParseStringSlice(technologiesStr.String)
+		}
+
+		// Instert data into JSON format
+		project := map[string]interface{}{
+			"id":           projectID,
+			"title":        title,
+			"description":  description,
+			"githubLink":   githubLink,
+			"languages":    languages,
+			"technologies": technologies,
+		}
+
 		projects = append(projects, project)
-	}
 
-	if err := rows.Err(); err != nil {
-		log.Fatalf("Error iterating over rows: %v", err)
-	}
+		if err := rows.Err(); err != nil {
+			log.Fatalf("Error iterating over rows: %v", err)
+		}
 
-	// Send Response
-	c.JSON(http.StatusOK, gin.H{
-		"projects": projects,
-	})
+		// Return Data
+		c.JSON(http.StatusOK, gin.H{
+			"projects": projects,
+		})
+	}
 }
 
 func openDatabase() *sql.DB {
