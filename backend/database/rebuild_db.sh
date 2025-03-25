e68c2f85e77a9254fb4f2635d2936895ccb4d0b3
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2025-03-25 16:34:43 +0000

    feat(database): update `rebuild_db.sh`
    Signed-off-by: Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>

--- backend/database/rebuild_db.sh
+++ backend/database/rebuild_db.sh
@@ -6,10 +6,42 @@ GREEN='\033[0;32m'
 CYAN='\033[0;36m'
 NC='\033[0m' # No Color
 
-database_file=database/portfolio.db
+database_directory_path=database
+
+# Parse options
+while getopts ":p:" opt; do
+  case $opt in
+    p) database_directory_path="$OPTARG" ;;
+    \?) echo "${RED}Invalid option -$OPTARG" >&2; echo "Use -p </path/to/database>${NC}"; exit 1 ;;
+  esac
+done
+
+# Shift the parsed options
+shift $((OPTIND-1))
+
+database_file=$database_directory_path/portfolio.db
+schema_file=$database_directory_path/schema.sql
+data_patch_directory=$database_directory_path/data
+
+# Check if portfolio.db exisits
+if [ ! -f "$database_file" ]; then
+  echo "${RED}'portfolio.db' doesn't exist in $database_directory_path" >&2
+  exit 1
+fi
+
+# Check if schema.sql exisits
+if [ ! -f "$schema_file" ]; then
+  echo "${RED}'schema.sql' doesn't exist in $database_directory_path" >&2
+  exit 1
+fi
+
+# Check if data directory exists
+if [ ! -d "$data_patch_directory" ]; then
+  echo "${RED}'/data' doesn't exist in $database_directory_path" >&2
+  exit 1
+fi
 
 # Run SQL script to rebuild schema
-schema_file=database/schema.sql
 echo "${CYAN}Rebuilding database schema${NC}"
 echo "  - Running schema: $schema_file"
 if ! output=$(sqlite3 "$database_file" < "$schema_file" 2>&1); then
@@ -22,7 +54,7 @@ fi
 echo ""
 
 # Run database patches to insert data
-for dir in database/data/*/; do
+for dir in $data_patch_directory/*/; do
   echo "${CYAN}Processing patch version: $dir${NC}"
   for sql_file in "$dir"*.sql; do
     if [ -f "$sql_file" ]; then
