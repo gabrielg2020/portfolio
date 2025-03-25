75cafd84ea6c5dc98b03f8e9194d37bfc8324d63
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2025-03-25 18:43:41 +0000

    feat(database): add data for experiences
    Signed-off-by: Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>

---
+++ backend/database/data/1.0.0/add_experiences.sql
@@ -0,0 +1,37 @@
+BEGIN TRANSACTION;
+
+-- 1 - DVSA
+INSERT INTO experiences (company, role, year_start, year_end, point_one, point_two, point_three )
+VALUES ('DVSA', 'Software Engineer', '2024', 'Current', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultricies placerat interdum. Vivamus sed velit mattis, mattis lectus sed, maximus dolor.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultricies placerat interdum. Vivamus sed velit mattis, mattis lectus sed, maximus dolor.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultricies placerat interdum. Vivamus sed velit mattis, mattis lectus sed, maximus dolor.');
+
+INSERT INTO experience_languages (experience_id, language_id, position)
+VALUES 
+    (1, 4, 1), -- php
+    (1, 2, 2); -- typescript
+
+INSERT INTO experience_technologies (experience_id, technology_id, position)
+VALUES 
+    (1, 2, 1), -- docker
+    (1, 4, 2), -- laminas
+    (1, 5, 3), -- aws
+    (1, 6, 4); -- mysql
+
+-- 2 - University of Roehampton
+INSERT INTO experiences (company, role, year_start, year_end, point_one, point_two, point_three )
+VALUES ('University of Roehampton', 'B.Sc. Digital Technology Solutions', '2024', 'Current', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultricies placerat interdum. Vivamus sed velit mattis, mattis lectus sed, maximus dolor.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultricies placerat interdum. Vivamus sed velit mattis, mattis lectus sed, maximus dolor.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultricies placerat interdum. Vivamus sed velit mattis, mattis lectus sed, maximus dolor.');
+
+INSERT INTO experience_languages (experience_id, language_id, position)
+VALUES 
+    (2, 5, 1), -- python
+    (2, 1, 2); -- go
+
+INSERT INTO experience_technologies (experience_id, technology_id, position)
+VALUES 
+    (2, 5, 1), -- aws
+    (2, 6, 2); -- mysql
+
+-- 3 - LEGO
+INSERT INTO experiences (company, role, year_start, year_end, point_one, point_two, point_three )
+VALUES ('LEGO', 'Retail Marketing Intern', '2022', '2022', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultricies placerat interdum. Vivamus sed velit mattis, mattis lectus sed, maximus dolor.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultricies placerat interdum. Vivamus sed velit mattis, mattis lectus sed, maximus dolor.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultricies placerat interdum. Vivamus sed velit mattis, mattis lectus sed, maximus dolor.');
+
+COMMIT;
--- backend/database/data/1.0.0/add_languages.sql
+++ backend/database/data/1.0.0/add_languages.sql
@@ -3,6 +3,8 @@ BEGIN TRANSACTION;
 INSERT INTO languages (name) VALUES 
   ('go'), -- 1
   ('typescript'), -- 2
-  ('vbnet'); -- 3
+  ('vbnet'), -- 3
+  ('php'), -- 4
+  ('python'); --5
 
 COMMIT;
