668ad4c114813c35a0004984c713ed58d0183875
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2025-03-29 23:53:32 +0000

    chore: reset frontend
    Signed-off-by: Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,25 +1,5 @@
 <script lang="ts">
-  import Contact from "./pages/Contact.svelte";
-  import Experiences from "./pages/Experiences.svelte";
   import Home from "./pages/Home.svelte";
-  import Projects from "./pages/Projects.svelte";
 </script>
 
 <Home />
-<Projects />
-<Experiences />
-<Contact />
-
-<style>
-  :global(html, body) {
-    height: 100%;
-    margin: 0;
-    padding: 0;
-    scroll-behavior: smooth; /* For smooth scrolling */
-  }
-
-  :global(body) {
-    overflow-y: scroll;
-    scroll-snap-type: y mandatory; /* Makes sections snap when scrolling */
-  }
-</style>
