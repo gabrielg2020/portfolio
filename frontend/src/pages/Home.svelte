2995927125550cf6065b9c1e39efafd1322eedce
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2025-03-30 02:24:54 +0100

    feat(frontend): add nav bar component
    Signed-off-by: Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>

--- frontend/src/pages/Home.svelte
+++ frontend/src/pages/Home.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   import Button from "../components/Button.svelte";
   import InfoTab from "../components/InfoTab.svelte";
+  import NavBar from "../components/NavBar.svelte";
 </script>
 
+<NavBar />
 <Button text="Website Docs"/>
 <InfoTab />
