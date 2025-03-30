4754aa4de355c50c9092b9314d336d05c2cc8add
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2025-03-30 00:52:07 +0000

    chore(frontend): track `button.svelte`
    Signed-off-by: Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>

---
+++ frontend/src/components/Button.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { ExternalLink, Download, MoveUpRight } from "@lucide/svelte";
+
+  export let text: string = "Text";
+  export let iconStr: string = "Link";
+  export let location: string = "home";
+  export let size:number = 15;
+  export let strokeWidth:number = 2.2;
+  $: icon = iconStr === "Link" ? ExternalLink : Download;
+</script>
+
+<a href="#{location}">
+  <div>
+    <span class="button">{text}</span>
+    <span class="icon"><svelte:component this={icon} size={size} strokeWidth={strokeWidth}/></span>
+  </div>
+</a>
+
+<style>
+  div {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    gap: 8px;
+    border: 2px solid var(--near-black);
+    padding: 4px;
+  }
+  span {
+    display: flex;
+    align-items: center;
+  }
+</style>
