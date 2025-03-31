04e58a753589fb6154d861f45e63869c794c9597
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2025-03-31 17:54:00 +0100

    feat(frontend): add button component
    Signed-off-by: Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>

---
+++ frontend/src/components/Button.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import { ArrowRight } from "@lucide/svelte";
+
+  export let text: string = "Button";
+  export let variant: string = "primary";
+  let isHovered = false;
+
+  const getVariantStyles = () => {
+    switch (variant) {
+      case "primary":
+        return "bg-blue-600 text-white hover:bg-blue-700";
+      case "blank":
+        return "bg-white text-gray-800 border border-gray-300 hover:bg-gray-50";
+    }
+  };
+
+  const getTextStyles = () => {
+    switch (variant) {
+      case "primary":
+        return "pl-4";
+      case "blank":
+        return "px-4";
+    }
+  };
+</script>
+
+<button
+  class="rounded-md flex items-center justify-center gap-2 font-medium transition-all duration-200 {getVariantStyles()}"
+  on:mouseenter={() => (isHovered = true)}
+  on:mouseleave={() => (isHovered = false)}
+>
+  <span class="text-base font-medium py-3 {getTextStyles()}">{text}</span>
+  {#if variant == "primary"}
+    <span class="pr-4 {isHovered ? "translate-x-1" : "translate-x-0"} transition-transform duration-200"
+      ><ArrowRight size="16" /></span
+    >
+  {/if}
+</button>
--- frontend/src/pages/Home.svelte
+++ frontend/src/pages/Home.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+  import Button from "../components/Button.svelte";
 </script>
 
 <div id="home">
   <h1 class="text-3xl font-bold underline font-sans">
     Home
   </h1>
+  <Button text="View Projects" variant="primary"/>
+  <Button text="View Resume" variant="blank"/>
 </div>
