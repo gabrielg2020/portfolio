61bfae3b32cbff20f5f43e5704cf8f13b1ac7d08
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2025-04-01 21:19:12 +0100

    feat(frontend): create callers for project and experience
    Signed-off-by: Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>

---
+++ frontend/src/scripts/callers/getExperience.ts
@@ -0,0 +1,27 @@
+export type Experience = {
+  type: string,
+  organisation: string,
+  role: string,
+  startYear: string,
+  endYear: string,
+  description: string,
+  languages: string[],
+  technologies: string[]
+}
+
+export async function GetExperiece(): Promise<Experience[]> {
+  let experiences: Experience[] = []
+
+  try {
+    const response = await fetch("/api/experience")
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+    const data = await response.json();
+    experiences = data.experiences;
+  } catch (err) {
+    console.error("Failed to fetch experiences:", err);
+  }
+
+  return experiences
+}
