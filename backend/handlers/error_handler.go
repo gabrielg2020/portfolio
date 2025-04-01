8850bb83e9f7c828e6377d24993c46cc9aa8aa2c
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2025-04-02 00:17:10 +0100

    fix(backend): handle nil errors
    Signed-off-by: Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>

--- backend/handlers/error_handler.go
+++ backend/handlers/error_handler.go
@@ -10,7 +10,15 @@ import (
 
 func handleInternalServerError(c *gin.Context, err error, logMessage string) {
 	logger.Errorf(logMessage, err)
-	c.JSON(http.StatusInternalServerError, gin.H{
-		"message": err.Error(),
-	})
+	// This condition is only hit when testing how frontend deals with errors
+	if err == nil {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"message": "Fake Erorr",
+		})
+		return
+	} else {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": err.Error(),
+		})
+	}
 }
