970f256647a4d03d8ed9bd76a5972edd16607941
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2025-05-05 18:46:12 +0100

    feat: create a util to unmarshal
    Signed-off-by: Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>

--- backend/handlers/projects_handler.go
+++ backend/handlers/projects_handler.go
@@ -1,27 +1,20 @@
 package handlers
 
 import (
-	"encoding/json"
 	"net/http"
-	"os"
 
 	"github.com/gabrielg2020/backend/models"
+	"github.com/gabrielg2020/backend/utils"
 	"github.com/gin-gonic/gin"
 )
 
 func GetProjects(c *gin.Context) {
 	var projects []models.Project
 
-	// Read JSON file
-	jsonContent, err := os.ReadFile("./data/projects.json")
-	if err != nil {
-		handleInternalServerError(c, err, "Failed to read projects.json: %v")
-	}
+	err := utils.UnmarshalJSON("./data/projects.json", &projects)
 
-	// Unmarshall JSON into projects
-	err = json.Unmarshal(jsonContent, &projects)
 	if err != nil {
-		handleInternalServerError(c, err, "Failed to unmashall projects.json: %v")
+		handleInternalServerError(c, err, "Failed to unmasall JSON: %v")
 	}
 
 	// Return Data
